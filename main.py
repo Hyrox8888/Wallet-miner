c1b7d64c864c9c4d6322069bc5980f37fcbdcd31
Hyro <104851284+Hyrox8888@users.noreply.github.com>
2022-05-19 16:27:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,180 @@
+from encodings import utf_8
+from msilib.schema import SelfReg
+from typing_extensions import Self
+import colorama, time, random
+import os
+os.system("title Hyrox fake miner[PAID]")
+colorama.init(autoreset=True)
+delay = 0.5
+def eth():
+	print(colorama.Fore.CYAN + """
+
+ ██░ ██▓██   ██▓ ██▀███   ▒█████  ▒██   ██▒
+▓██░ ██▒▒██  ██▒▓██ ▒ ██▒▒██▒  ██▒▒▒ █ █ ▒░
+▒██▀▀██░ ▒██ ██░▓██ ░▄█ ▒▒██░  ██▒░░  █   ░
+░▓█ ░██  ░ ▐██▓░▒██▀▀█▄  ▒██   ██░ ░ █ █ ▒ 
+░▓█▒░██▓ ░ ██▒▓░░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒
+ ▒ ░░▒░▒  ██▒▒▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░
+ ▒ ░▒░ ░▓██ ░▒░   ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░
+ ░  ░░ ░▒ ▒ ░░    ░░   ░ ░ ░ ░ ▒   ░    ░  
+ ░  ░  ░░ ░        ░         ░ ░   ░    ░  
+        ░ ░                                  
+									#MADE BY HYROX#8888""")
+	print(colorama.Fore.CYAN + "LOADING | 10%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 20%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 30%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 40%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 50%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 60%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 70%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 80%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 90%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 100%")
+	time.sleep(delay)
+	print(colorama.Fore.GREEN + "CRACKING API DONE")
+	time.sleep(delay)
+	print(colorama.Fore.YELLOW + "Welcome to the premium version")
+	time.sleep(delay)
+	print(colorama.Fore.YELLOW + "REMEMBER THIS IS FAKE!")
+	time.sleep(delay)
+	choice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	while True:
+           r1 = random.choices(choice, k=64)
+           r2 = random.randint(999, 9999)
+           r3 = random.randrange(999, 9999)
+           r4 = random.randrange(999, 9999)
+           r5 = random.randrange(999, 9999)
+           r6 = random.randrange(999, 9999)
+           ra = r4
+           rb = r4 + r5
+           rc = r4 + r5 + r6
+        
+           r2 = str(r2)
+           if "105" not in r2:
+            print(colorama.Fore.RED + "[0.000 ETH]" + colorama.Fore.RED + " | " + colorama.Fore.RED + "".join(r1) + colorama.Fore.RED + " |")
+           if "105" in r2:
+            print(colorama.Fore.GREEN + f"Found 0.{r3} ETH in Wallet: | " + "".join(r1))
+            time.sleep(1)
+            print(colorama.Fore.GREEN + "Etherium Being Sent To Anonymising Service")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {ra} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {rb} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {rc} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymised Etherium Succsesfully Sent To Main Address [0.{r3} ETH]")
+            time.sleep(5)
+            print(colorama.Fore.CYAN + "Continue Mining")
+            time.sleep(2)
+           time.sleep(0.05)
+def btc():
+	print(colorama.Fore.CYAN + """
+
+ ██░ ██▓██   ██▓ ██▀███   ▒█████  ▒██   ██▒
+▓██░ ██▒▒██  ██▒▓██ ▒ ██▒▒██▒  ██▒▒▒ █ █ ▒░
+▒██▀▀██░ ▒██ ██░▓██ ░▄█ ▒▒██░  ██▒░░  █   ░
+░▓█ ░██  ░ ▐██▓░▒██▀▀█▄  ▒██   ██░ ░ █ █ ▒ 
+░▓█▒░██▓ ░ ██▒▓░░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒
+ ▒ ░░▒░▒  ██▒▒▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░
+ ▒ ░▒░ ░▓██ ░▒░   ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░
+ ░  ░░ ░▒ ▒ ░░    ░░   ░ ░ ░ ░ ▒   ░    ░  
+ ░  ░  ░░ ░        ░         ░ ░   ░    ░  
+        ░ ░                                 
+									#MADE BY HYROX#8888""")
+	print(colorama.Fore.CYAN + "LOADING | 10%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 20%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 30%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 40%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 50%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 60%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 70%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 80%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 90%")
+	time.sleep(delay)
+	print(colorama.Fore.CYAN + "LOADING | 100%")
+	time.sleep(delay)
+	print(colorama.Fore.GREEN + "CRACKING API DONE")
+	time.sleep(delay)
+	print(colorama.Fore.YELLOW + "Welcome to the premium version")
+	time.sleep(delay)
+	print(colorama.Fore.YELLOW + "REMEMBER THIS IS FAKE!")
+	time.sleep(delay)
+	choice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	while True:
+           r1 = random.choices(choice, k=64)
+           r2 = random.randint(100, 600)
+           r3 = random.randrange(999, 999999)
+           r4 = random.randrange(999, 999999)
+           r5 = random.randrange(999, 9999)
+           r6 = random.randrange(999, 9999)
+           ra = r4
+           rb = r4 + r5
+           rc = r4 + r5 + r6
+        
+           r2 = str(r2)
+           if "105" not in r2:
+            print(colorama.Fore.RED + "[0.000 BTC]" + colorama.Fore.RED + " | " + colorama.Fore.RED + "".join(r1) + colorama.Fore.RED + " |")
+           if "105" in r2:
+            print(colorama.Fore.GREEN + f"Found 0.{r3} BTC in Wallet: | " + "".join(r1))
+            time.sleep(1)
+            print(colorama.Fore.GREEN + "Bitcoin Being Sent To Anonymising Service")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {ra} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {rb} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymising over {rc} Wallets")
+            time.sleep(3)
+            print(colorama.Fore.GREEN + f"Anonymised Bitcoin Succsesfully Sent To Main Address [0.{r3} ETH]")
+            time.sleep(5)
+            print(colorama.Fore.CYAN + "Continue Mining")
+            time.sleep(2)
+           time.sleep(0.05)
+def cred():
+	print(colorama.Fore.CYAN + """
+
+ ██░ ██▓██   ██▓ ██▀███   ▒█████  ▒██   ██▒
+▓██░ ██▒▒██  ██▒▓██ ▒ ██▒▒██▒  ██▒▒▒ █ █ ▒░
+▒██▀▀██░ ▒██ ██░▓██ ░▄█ ▒▒██░  ██▒░░  █   ░
+░▓█ ░██  ░ ▐██▓░▒██▀▀█▄  ▒██   ██░ ░ █ █ ▒ 
+░▓█▒░██▓ ░ ██▒▓░░██▓ ▒██▒░ ████▓▒░▒██▒ ▒██▒
+ ▒ ░░▒░▒  ██▒▒▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒▒ ░ ░▓ ░
+ ▒ ░▒░ ░▓██ ░▒░   ░▒ ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░
+ ░  ░░ ░▒ ▒ ░░    ░░   ░ ░ ░ ░ ▒   ░    ░  
+ ░  ░  ░░ ░        ░         ░ ░   ░    ░  
+        ░ ░                                 
+									#MADE BY HYROX#8888""")
+	print(colorama.Fore.CYAN + "Made by hyrox#8888 on discord")
+	print(colorama.Fore.CYAN + "Remember wallet miner doesnt exist")
+print("""Pick One
+1. Etherium Stealer
+2. Bitcoin Stealer""")
+while True:
+	main = input("> ")
+	if main == "1" or main == "2":
+		break
+	else:
+		print("Enter A Valid Choice")
+if main == "1":
+	eth()
+if main == "2":
+	btc()
+
